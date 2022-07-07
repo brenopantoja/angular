9995b810f3265b3c6896250a498e5cbc471aa89f
Breno Pantoja <breno.r.pantoja@gmail.com>
2022-07-07 15:08:29 -0300

    07072022 3 30pm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 
 const routes: Routes = [// It is defining router
-{path: '', component:HomeComponent}
-
+{path: '', component:HomeComponent},
+{path: 'about', component:AboutComponent },
 ];
 
 
